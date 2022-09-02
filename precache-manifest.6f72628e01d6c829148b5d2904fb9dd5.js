dd88adcba8ebf222f16c00e0868f0842e19a7b4e
Shrayansh Jain <jainshrayansh1998@gmail.com>
2022-09-02 18:20:23 +0530

    Updates

---
+++ precache-manifest.6f72628e01d6c829148b5d2904fb9dd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754475a2e1a6ed4e82a4206966a38de0",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "619f4fce3bae995dadca",
+    "url": "/emojipedia/static/css/main.70b3bdf7.chunk.css"
+  },
+  {
+    "revision": "be8c9388e0c2bb835496",
+    "url": "/emojipedia/static/js/2.bc581622.chunk.js"
+  },
+  {
+    "revision": "619f4fce3bae995dadca",
+    "url": "/emojipedia/static/js/main.6a335cf8.chunk.js"
+  },
+  {
+    "revision": "3ad02f26d7c433e7efca",
+    "url": "/emojipedia/static/js/runtime-main.d1c65f91.js"
+  }
+]);
